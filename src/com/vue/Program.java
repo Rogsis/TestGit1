9848ed47340f39bd4728f439703dd10fb836cb04
chapelet-k <chapelet-k@acr-m4630g-302.tec.saint-louis.local>
2015-10-15 09:31:40 +0200

    1ere modification

---
+++ src/com/vue/Program.java
@@ -0,0 +1,11 @@
+package com.vue;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World !");
+		System.out.println("Antoine est lourd");
+	}
+
+}
